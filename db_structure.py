fc8f5ba3fbd31a603cafe688ba8ba1f6871c40b6
FantasiA10 <yw4177@nyu.edu>
2023-03-26 23:59:35 -0400

    update dbstructure

--- db_structure.py
+++ db_structure.py
@@ -27,3 +27,32 @@ response = requests.post("https://project-finder.herokuapp.com/projects/details/
 print(response.json())
 #data = requests.get("https://project-finder.herokuapp.com/projects/dict")
 #print(data.json())
+
+def get_file_name(name):
+    """
+    get the name of the file
+    """
+    response = requests.get(URL+PROJECT_GET_FILE+f'/{name}'+'/0')
+    if response.status_code == 200:
+        the_dict = response.json()
+        """
+        Check if the project has uploaded file
+        """
+        if 'filename' in the_dict:
+            return response.json()['filename']
+        return None
+    return None
+
+
+def get_file(name):
+    """
+    get the file for download
+    """
+    response = requests.get(URL+PROJECT_GET_FILE+f'/{name}'+'/1')
+    filename = response.headers.get('content-disposition', '').split('"')[-2]
+    file_mimetype, encoding  = mimetypes.guess_type(filename)
+    return send_file(response.content,
+                     as_attachment=True,
+                     attachment_filename=filename,
+                     mimetype=file_mimetype
+                     )
