afebca1a5a59bf8bcff5424333de1134d6827d7e
xieshangyu <76835319+xieshangyu@users.noreply.github.com>
2023-04-08 22:38:31 -0400

    debug face recognition

--- web_page_spring/face/app.py
+++ web_page_spring/face/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 import face_recognition
 import base64
 from io import BytesIO
@@ -8,22 +8,23 @@ app = Flask(__name__)
 
 # Simulate user database
 USER_DATABASE = {
-    "user1": {
-        "name": "John Doe",
-        "face_encoding": [-0.0980, 0.0632, 0.0265, ..., 0.0639, -0.0294, -0.0087]
-    },
-    "user2": {
-        "name": "Jane Smith",
-        "face_encoding": [0.0123, -0.0456, 0.0674, ..., -0.0345, 0.0229, -0.0671]
-    }
+    # Existing user data
 }
 
-@app.route('/login', methods=['POST'])
-def login():
+@app.route('/')
+def index():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/register_user', methods=['POST'])
+def register_user():
     data = request.get_json()
 
-    if not data or "image" not in data:
-        return jsonify({"success": False, "message": "Image data missing"}), 400
+    if not data or "image" not in data or "name" not in data:
+        return jsonify({"success": False, "message": "Image or name data missing"}), 400
 
     # Decode image data from Base64
     image_data = base64.b64decode(data["image"].split(',')[1])
@@ -41,13 +42,16 @@ def login():
     # Extract facial features
     face_encodings = face_recognition.face_encodings(face_image, face_locations)
 
-    # Iterate through the user database to find a user that matches the extracted facial features
-    for user_id, user_data in USER_DATABASE.items():
-        match = face_recognition.compare_faces([user_data["face_encoding"]], face_encodings[0], tolerance=0.6)
-        if match[0]:
-            return jsonify({"success": True, "user": {"id": user_id, "name": user_data["name"]}})
+    # Save the user's facial features and name in the database
+    user_id = f"user{len(USER_DATABASE) + 1}"
+    USER_DATABASE[user_id] = {
+        "name": data["name"],
+        "face_encoding": face_encodings[0]
+    }
 
-    return jsonify({"success": False, "message": "Login failed"}), 401
+    return jsonify({"success": True, "message": "User registered successfully"})
+
+# Existing login() function
 
 if __name__ == "__main__":
     app.run(debug=True)
