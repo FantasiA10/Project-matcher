bf01af8dd88c01d758f4a9ff50353ccd3cd29cfd
xieshangyu <76835319+xieshangyu@users.noreply.github.com>
2023-04-16 04:02:01 -0400

    add register function

--- web_page_spring/face/app.py
+++ web_page_spring/face/app.py
@@ -44,5 +44,28 @@ def login():
 
     return jsonify(success=False)
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        img_data = request.form['image'].split(',', 1)[1]
+        img_bytes = base64.b64decode(img_data)
+        name = request.form['name']
+
+        # Save the image to known_faces folder
+        img_path = os.path.join(face_folder, f'{name}.jpg')
+        with open(img_path, 'wb') as f:
+            f.write(img_bytes)
+
+        # Load the image and save its encoding to known_faces dictionary
+        img = face_recognition.load_image_file(img_path)
+        encodings = face_recognition.face_encodings(img)
+        if len(encodings) > 0:
+            known_faces[name] = encodings[0]
+
+        return jsonify(success=True)
+
+    return render_template('register.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
